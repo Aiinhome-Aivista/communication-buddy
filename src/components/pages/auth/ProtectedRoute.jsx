2f14ddda98d8d2af6eb3fc8cb207368f7bc2bfc8
Himanshu <294himangshu@gmail.com>
2025-10-09 21:14:50 +0530

    app routes fix

--- src/components/pages/auth/ProtectedRoute.jsx
+++ src/components/pages/auth/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import { Navigate } from 'react-router';
+import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../../provider/AuthProvider';
 
 export default function ProtectedRoute({ children }) {
