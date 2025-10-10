00e56fe03bdfb67121ebb8f1e4b953fea7e6b491
Pinki Paul <pp3785828@gmail.com>
2025-10-10 16:36:40 +0530

    practice and test page

--- src/components/pages/practiceTest/PracticeTest.jsx
+++ src/components/pages/practiceTest/PracticeTest.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
 
 const testData = [
-  {
-    title: "Python",
-    duration: "10 mins",
-    date: "17/10/2025",
-    status: "Active",
-  },
+  { title: "Python", duration: "10 mins", date: "17/10/2025", status: "Active" },
   { title: "SQL", duration: "22 mins", date: "11/10/2025", status: "Active" },
   { title: "JAVA", duration: "7 mins", date: "13/10/2025", status: "Active" },
 ];
@@ -20,117 +20,140 @@ export default function PracticeTest() {
   const [testType, setTestType] = useState("Test Type");
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  return (
-          <div className="w-full h-full bg-gray-50 flex flex-col">
-            <div className="flex-grow flex flex-col">
-              <div className="pt-6 px-6 ">
-                <h1 className="text-2xl font-bold text-[#2C2E42]">
-                  Practice &amp; Test
-                </h1>
-                <div className="flex flex-row items-center mt-6 space-x-8 gap-4">
-                  <div className="flex border border-[#BCC7D2] rounded-xl overflow-hidden">
-                    {tabOptions.map((tab) => (
-                      <button
-                        key={tab}
-                        className={`px-6 py-2 text-sm font-medium ${activeTab === tab
-                          ? "bg-[#FEFEFE] text-[#2C2E42]"
-                          : "bg-gray-100 text-[#8F96A9]"
-                          }`}
-                        onClick={() => setActiveTab(tab)}
-                      >
-                        {tab}
-                      </button>
-                    ))}
-                  </div>
-                  <input
-                    type="text"
-                    placeholder="Search content"
-                    className="flex-1 px-6 py-2 border border-[#BCC7D2] rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-blue-200 bg-white"
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                  />
-                  <div className="relative ml-auto">
-                    <button
-                      className="border border-[#BCC7D2] rounded-xl px-8 py-2 text-sm bg-white flex items-center justify-between w-80"
-                      onClick={() => setDropdownOpen(!dropdownOpen)}
-                    >
-                      {testType}
-                      <svg
-                        className="w-4 h-4 ml-2"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M19 9l-7 7-7-7"
-                        />
-                      </svg>
-                    </button>
-                    {dropdownOpen && (
-                      <ul className="absolute mt-1 left-0 w-80 bg-white  rounded-xl shadow-md z-10">
-                        {testTypeOptions.map((option) => (
-                          <li
-                            key={option}
-                            onClick={() => {
-                              setTestType(option);
-                              setDropdownOpen(false);
-                            }}
-                            className="px-4 py-2 text-sm hover:bg-gray-100 cursor-pointer"
-                          >
-                            {option}
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </div>
-                </div>
+  const filteredData = testData.filter((test) =>
+    Object.values(test).some((value) =>
+      value.toString().toLowerCase().includes(search.toLowerCase())
+    )
+  );
 
-                <div className="bg-[#FFFFFF] mt-6 rounded-xl shadow">
-                  <table className="w-full ">
-                    <thead>
-                      <tr>
-                        <th className="px-2 py-3 text-left text-xs font-medium text-[#3D5B81]">
-                          Test Title
-                        </th>
-                        <th className="px-25 py-3 text-xs font-medium text-[#3D5B81]">
-                          Session Duration
-                        </th>
-                        <th className="px-10 py-3 text-xs font-medium text-[#3D5B81]">
-                          Session Date
-                        </th>
-                        <th className="px-6 p-3 text-xs font-medium text-[#3D5B81]">
-                          Action
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {testData.map((test, idx) => (
-                        <tr key={test.title} className="hover:bg-gray-50">
-                          <td className="px-2 py-4 text-sm text-[#3D5B81]">
-                            {test.title}
-                          </td>
-                          <td className="px-100 py-4 text-sm text-[#29324173]">
-                            {test.duration}
-                          </td>
-                          <td className="px-10 py-4 text-sm text-[#29324173]">
-                            {test.date}
-                          </td>
-                          <td className="px-6 py-4 text-sm">
-                            <span className="px-3 py-1 rounded-full text-xs font-medium bg-green-100 text-[#46BA2F]">
-                              {test.status}
-                            </span>
-                          </td>
-                          <td></td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              </div>
+
+  // ✅ Custom template for status badge
+  const statusBodyTemplate = (rowData) => (
+    <span
+      className={`px-3 py-1 rounded-full text-xs font-medium ${rowData.status === "Active"
+        ? "bg-[#35FF021A] text-[#46BA2F]"
+        : "bg-gray-100 text-gray-500"
+        }`}
+    >
+      {rowData.status}
+    </span>
+  );
+
+
+  return (
+    <div className="w-full h-full bg-[#ECEFF2] flex flex-col">
+      <div className="flex-grow flex flex-col">
+        <div className="pt-4 px-6">
+          <h1 className="text-2xl font-bold text-[#2C2E42]">
+            Practice & Test
+          </h1>
+
+          {/* Tabs + Search + Dropdown */}
+          <div className="flex flex-row items-center mt-6 space-x-8 gap-4">
+            <div className="flex border border-[#BCC7D2] rounded-xl overflow-hidden h-[45px]">
+              {tabOptions.map((tab) => (
+                <button
+                  key={tab}
+                  className={`px-6 py-2 text-sm text-semibold rounded-xl font-medium ${activeTab === tab
+                    ? "bg-[#FEFEFE] text-[#2C2E42]"
+                    : "bg-[#ECEFF2] text-[#8F96A9]"
+                    }`}
+                  onClick={() => setActiveTab(tab)}
+                >
+                  {tab}
+                </button>
+              ))}
+            </div>
+
+
+
+            <div className="relative flex-1">
+              <input
+                type="text"
+                placeholder="Search content"
+                className="w-full h-[45px] px-6 py-2 border border-[#BCC7D2] rounded-xl text-[#8F96A9] text-sm focus:outline-none focus:ring-2 focus:ring-gray-200 bg-[#ECEFF2] pr-12"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <img
+                src="public/assets/icons/search.svg"
+                alt="Search"
+                className="absolute right-4 top-1/2 transform -translate-y-1/2 w-5 h-5 pointer-events-none"
+              />
+            </div>
+            {/* Test type */}
+            <div className="relative ml-auto">
+              <button
+                className="border border-[#BCC7D2] rounded-xl px-8 py-2 text-sm bg-[#ECEFF2] flex items-center justify-between w-80 h-[45px]"
+                onClick={() => setDropdownOpen(!dropdownOpen)}
+                style={{ color: "#8F96A9" }}
+              >
+
+                {testType}
+                {dropdownOpen ? (
+                  <KeyboardArrowUpIcon className="w-4 h-4 text-[#8F96A9]" />
+                ) : (
+                  <KeyboardArrowDownIcon className="w-4 h-4 text-[#8F96A9]" />
+                )}
+
+              </button>
+              {dropdownOpen && (
+                <ul className="absolute mt-1 left-0 w-80 bg-[#BCC7D2] rounded-2xl shadow-md z-10">
+                  {testTypeOptions.map((option) => (
+                    <li
+                      key={option}
+                      onClick={() => {
+                        setTestType(option);
+                        setDropdownOpen(false);
+                      }}
+                      className={`px-4 py-2 text-base cursor-pointer font-medium text-[#182938]
+      ${testType === option
+                          ? "bg-[#BCC7D2] font-bold"
+                          : "hover:bg-[#D9D9D9]"
+                        }
+    `}
+                    >
+                      {option}
+                    </li>
+                  ))}
+
+                </ul>
+              )}
             </div>
           </div>
+
+          {/*DataTable */}
+          <div className="table-body custom-width-table">
+            <DataTable
+              value={filteredData}
+              paginator
+              rows={5}
+              rowsPerPageOptions={[3, 5, 10]}
+              paginatorClassName="!m-0 !border-t"
+              emptyMessage="No APIs found"
+
+            >
+              <Column
+                field="title"
+                header="Test Title"
+                body={(rowData) => (
+                  <span style={{ color: "#3D5B81", fontWeight: "400" }}>
+                    {rowData.title}
+                  </span>
+                )}
+              ></Column>
+
+              <Column field="duration" header="Session Duration"></Column>
+              <Column field="date" header="Session Date"></Column>
+              <Column
+                field="status"
+                header="ACTION"
+                body={statusBodyTemplate}
+              ></Column>
+            </DataTable>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/pages/scheduleSession/ScheduleSession.jsx
+++ src/components/pages/scheduleSession/ScheduleSession.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import SessionModal from "../../modal/SessionModal";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const testData = [
   { title: "Python", date: "17/10/2025", status: "Active" },
@@ -10,6 +14,7 @@ const testData = [
 const tabOptions = ["Upcoming", "Ongoing", "Expired"];
 const testTypeOptions = ["All", "Technology", "Communication"];
 
+
 export default function ScheduleSession() {
   const [activeTab, setActiveTab] = useState("Upcoming");
   const [search, setSearch] = useState("");
@@ -18,9 +23,27 @@ export default function ScheduleSession() {
   const [modalOpen, setModalOpen] = useState(false);
   const [sessionDuration, setSessionDuration] = useState(15);
 
+
+  const filteredData = testData.filter((test) =>
+    Object.values(test).some((value) =>
+      value.toString().toLowerCase().includes(search.toLowerCase())
+    )
+  );
+
+  const statusBodyTemplate = (rowData) => (
+    <span
+      className={`px-3 py-1 rounded-full text-xs font-medium ${rowData.status === "Active"
+          ? "bg-[#35FF021A] text-[#46BA2F]"
+          : "bg-gray-100 text-gray-500"
+        }`}
+    >
+      {rowData.status}
+    </span>
+  );
+
   return (
-    <div className="w-screen h-screen bg-gray-50 flex flex-col relative">
-      <div className="flex-grow flex flex-col pt-6 px-6">
+    <div className="w-[100%] h-[100%] bg-[#ECEFF2] flex flex-col relative">
+      <div className="flex-grow flex flex-col pt-4 px-6">
         {/* Header section with button on right */}
         <div className="flex justify-between items-center">
           <h1 className="text-2xl font-bold text-[#2C2E42]">
@@ -44,109 +67,98 @@ export default function ScheduleSession() {
         </div>
 
         <div className="flex flex-row items-center mt-6 space-x-8 gap-4">
-          <div className="flex border border-[#BCC7D2] rounded-xl overflow-hidden">
+          <div className="flex border border-[#BCC7D2] rounded-xl overflow-hidden h-[45px]">
             {tabOptions.map((tab) => (
               <button
                 key={tab}
-                className={`px-6 py-2 text-sm font-medium ${
-                  activeTab === tab
+                className={`px-6 py-2 text-sm  text-semibold rounded-xl font-medium ${activeTab === tab
                     ? "bg-[#FEFEFE] text-[#2C2E42]"
-                    : "bg-gray-100 text-[#8F96A9]"
-                }`}
+                    : "bg-[#ECEFF2] text-[#8F96A9]"
+                  }`}
                 onClick={() => setActiveTab(tab)}
               >
                 {tab}
               </button>
             ))}
           </div>
-          <input
-            type="text"
-            placeholder="Search content"
-            className="flex-1 px-6 py-2 border border-[#BCC7D2] rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-blue-200 bg-white"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
+
+
+          <div className="relative flex-1">
+            <input
+              type="text"
+              placeholder="Search content"
+              className="w-full h-[45px] px-6 py-2 border border-[#BCC7D2] rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-gray-200 bg-[#ECEFF2] pr-12"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <img
+              src="public/assets/icons/search.svg"
+              alt="Search"
+              className="absolute right-4 top-1/2 transform -translate-y-1/2 w-5 h-5 pointer-events-none"
+            />
+          </div>
           <div className="relative ml-auto">
             <button
-              className="border border-[#BCC7D2] rounded-xl px-8 py-2 text-sm bg-white flex items-center justify-between w-80"
+              className="border border-[#BCC7D2] rounded-xl px-8 py-2 text-sm bg-[#ECEFF2] flex items-center justify-between w-80 h-[45px]"
               onClick={() => setDropdownOpen(!dropdownOpen)}
+               style={{ color: "#8F96A9" }} 
             >
               {testType}
-              <svg
-                className="w-4 h-4 ml-2"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
+              
+               {dropdownOpen ? (
+                  <KeyboardArrowUpIcon className="w-4 h-4 text-[#8F96A9]" />
+                ) : (
+                  <KeyboardArrowDownIcon className="w-4 h-4 text-[#8F96A9]" />
+                )}
+
             </button>
             {dropdownOpen && (
-              <ul className="absolute mt-1 left-0 w-80 bg-[#B8C6D6] rounded-2xl shadow-md z-10">
-                {testTypeOptions.map((option, idx) => (
+              <ul className="absolute mt-1 left-0 w-80 bg-[#BCC7D2] rounded-2xl shadow-md z-10">
+                {testTypeOptions.map((option) => (
                   <li
                     key={option}
                     onClick={() => {
                       setTestType(option);
                       setDropdownOpen(false);
                     }}
-                    className={`px-4 py-2 text-sm cursor-pointer text-[#1A2530] rounded-xl
-      ${
-        idx === 1
-          ? "bg-[#D9D9D9] font-semibold"
-          : testType === option
-          ? "bg-[#BCC7D2] font-semibold"
-          : "hover:bg-[#A9B7C6]"
-      }
+                    className={`px-4 py-2 text-base font-medium cursor-pointer text-[#1A2530]
+      ${testType === option
+                        ? "bg-[#BCC7D2] font-semibold"
+                        : "hover:bg-[#D9D9D9]"
+                      }
     `}
                   >
                     {option}
                   </li>
                 ))}
+
               </ul>
             )}
           </div>
         </div>
 
-        {/* Table */}
-        <div className="flex-1 bg-[#FFFFFF] mt-6 rounded-xl shadow">
-          <table className="w-full ">
-            <thead>
-              <tr>
-                <th className="px-2 py-3 text-left text-xs font-medium text-[#3D5B81]">
-                  Test Title
-                </th>
-                <th className="text-right text-xs font-medium text-[#3D5B81]">
-                  Session Date
-                </th>
-                <th className="px-5 text-right text-xs font-medium text-[#3D5B81]">
-                  ACTION
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {testData.map((test, idx) => (
-                <tr key={test.title} className="hover:bg-gray-50">
-                  <td className="px-2 py-4 text-sm text-[#3D5B81]">
-                    {test.title}
-                  </td>
-                  <td className="px-2 py-4 text-sm text-right text-[#29324173]">
-                    {test.date}
-                  </td>
-                  <td className="px-1 py-4 text-sm text-right ">
-                    <span className="px-4 py-1 rounded-full text-xs text-right font-medium bg-green-100 text-[#46BA2F]">
-                      {test.status}
-                    </span>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+
+        {/* DataTable */}
+        <div className="table-body custom-width-table">
+          <DataTable
+            value={filteredData}
+            paginator
+            rows={5}
+            rowsPerPageOptions={[3, 5, 10]}
+            paginatorClassName="!m-0 !border-t"
+            emptyMessage="No tests found"
+           
+          >
+            <Column
+              field="title"
+              header="Test Title"
+              body={(rowData) => (
+                <span className="text-[#3D5B81] font-medium">{rowData.title}</span>
+              )}
+            />
+            <Column field="date" header="Session Date" />
+            <Column field="status" header="ACTION" body={statusBodyTemplate} />
+          </DataTable>
         </div>
       </div>
       <SessionModal
