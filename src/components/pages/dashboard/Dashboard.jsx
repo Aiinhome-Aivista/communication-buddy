780ffb9fd2f87a72d78a526a73cc941a2f272bde
Himanshu <294himangshu@gmail.com>
2025-10-10 21:57:40 +0530

    hr dashboard api bind

--- src/components/pages/dashboard/Dashboard.jsx
+++ src/components/pages/dashboard/Dashboard.jsx
@@ -2,11 +2,12 @@ import HrDashboard from './HrDashboard';
 import CandidateDashboard from './CandidateDashboard';
 
 export default function Dashboard() {
+  const role = typeof window !== 'undefined' ? sessionStorage.getItem('userRole') : null;
   return (
     <>
       <section className="dashboard-container h-full">
         <div className="dashboard-layout flex">
-          <CandidateDashboard/>
+          {role === 'hr' ? <HrDashboard /> : <CandidateDashboard />}
           {/* <ScheduleSession/> */}
           {/* <PracticeTest/> */}
         </div>
