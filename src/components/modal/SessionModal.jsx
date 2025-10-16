3b69dba0188af98bb24ffc657c36ce4b599f626c
Bibekananda Mahata <bibekmahata112@gmail.com>
2025-10-16 17:42:04 +0530

    fix

--- src/components/modal/SessionModal.jsx
+++ src/components/modal/SessionModal.jsx
@@ -144,7 +144,7 @@ export default function SessionModal({
       );
       if (
         response &&
-        (response.message === "Topic assigned successfully and emails sent" ||
+        (
           response.success === true ||
           response.status === 200)
       ) {
