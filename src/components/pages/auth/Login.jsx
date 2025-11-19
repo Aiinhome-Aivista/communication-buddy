caf7491fc12624649659626ae724abc3b0be9c18
Pinki Paul <pp3785828@gmail.com>
2025-11-19 15:26:46 +0530

    colour change

--- src/components/pages/auth/Login.jsx
+++ src/components/pages/auth/Login.jsx
@@ -77,7 +77,7 @@ function Login() {
       )}
       <p
         className="w-full flex text-base md:text-lg cursor-pointer">
-        A<span className="text-[#DFB916]">ii</span>nhome
+        A<span className="text-[#84840f]">ii</span>nhome
         <span className="px-1">|</span>
         <span className="font-bold">CB</span>
       </p>
@@ -107,7 +107,8 @@ function Login() {
               <span className="px-1">|</span>
               <span className="font-bold">CB</span>
             </p>
-            <p className="text-xs text-[#2C2E42]">Communication Buddy V2.5</p>
+            <p className="text-xs text-[#2C2E42]">CommBuddy v2.5</p>
+            <p className="text-xs text-[#2C2E42]/80 font-semibold mt-1">Soft Skill Development Platform</p>
           </div>
           <div className="w-full flex items-center justify-center px-6 pt-5">
             <img
